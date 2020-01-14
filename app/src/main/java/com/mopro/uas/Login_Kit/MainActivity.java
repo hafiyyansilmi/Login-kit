2e6fe71c5edd5b42bd7dd6a4c1e57dc2ba139cc4
Mochamad Rizky Purnama <rizkylens@gmail.com>
2020-01-14 21:23:01 +0700

    add function fb login

--- app/src/main/java/com/mopro/uas/Login_Kit/MainActivity.java
+++ app/src/main/java/com/mopro/uas/Login_Kit/MainActivity.java
@@ -4,132 +4,113 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.appevents.AppEventsLogger;
 
 
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.mopro.uas.test.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-private Button button, button1, button2, button3;
+private Button fb_button, button1, button2, button3;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
+//        FacebookSdk.sdkInitialize(getApplicationContext());
+//        AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_main);
 
-//        final MediaPlayer salaamMP = MediaPlayer.create(this, R.raw.salaam);
-//        salaamMP.start();
-//        final MediaPlayer bismillahmMP = MediaPlayer.create(this, R.raw.bismillah);
+
 
         final Animation nextPagePop = AnimationUtils.loadAnimation(this, R.anim.nextpage_pop);
 
-        button  = findViewById(R.id.button);
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        callbackManager = CallbackManager.Factory.create();
+        this.setTitle("Login Sample");
+        LoginButton fb_button = (LoginButton)findViewById(R.id.fb_button);
+        fb_button.setReadPermissions(Arrays.asList("email","public_profile"));
+        AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
             @Override
-            public void onClick(View view) {
-//                bismillahmMP.start();
-                view.startAnimation(nextPagePop);
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
 
-                view.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
+            }
+        };
+        ProfileTracker profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
 
-                        openAlphabetPage();
+            }
+        };
+        fb_button.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+                Profile profile = Profile.getCurrentProfile();
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                Log.e("aa", ""+response.toString());
+                                try {
+                                    Toast.makeText(getApplicationContext(), "Hi, " + object.getString("name"), Toast.LENGTH_LONG).show();
+                                } catch(JSONException ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
 
-                    }
-                }, 50);
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplication(), R.string.cancel_login, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getApplication(), R.string.error_login, Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                bismillahmMP.start();
-                view.startAnimation(nextPagePop);
-
-                view.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-
-                        openSambungPage();
-
-                    }
-                }, 50);
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                bismillahmMP.start();
-                view.startAnimation(nextPagePop);
-
-                view.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-
-                        openLarangPage();
-
-                    }
-                }, 50);
-            }
-        });
-
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                bismillahmMP.start();
-                view.startAnimation(nextPagePop);
-
-                view.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-
-                        openBerkaitanPage();
-
-                    }
-                }, 50);
-            }
-        });
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
 
     }
 
-    public void openAlphabetPage(){
-        Intent intent = new Intent(this, AlphabetPage.class);
-        startActivity(intent);
-
-    }
-
-    public void openSambungPage(){
-        Intent intent = new Intent(this, SambungPage.class);
-        startActivity(intent);
-
-    }
-
-    public void openLarangPage(){
-        Intent intent = new Intent(this, LarangPage.class);
-        startActivity(intent);
-
-    }
-
-    public void openBerkaitanPage(){
-        Intent intent = new Intent(this, BerkaitanPage.class);
-        startActivity(intent);
-
-    }
 
 }
